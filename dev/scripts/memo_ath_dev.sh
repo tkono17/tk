4f93f20add54dd62908fc8692fb2080f0c90f83c
tkohno <takanori.kono@cern.ch>
2019-07-12 10:02:18 +0200

    Add memo scripts

---
+++ dev/scripts/memo_ath_dev.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+1. Release setup
+
+asetup AnalysisBase,21.2.75
+
+21.0: Tier-0 release
+21.1: HLT at Point1
+21.2: Analysis and derivation
+21.3: Simulation development
+21.6: Event generation
+
+2. On every login
+
+cd ../build
+asetup --restore
+source x86_64-*/setup.sh
+
---
+++ dev/scripts/memo_ath_git.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env zsh
+
+echo """Git workflow in Athena
+
+0. Setup
+
+setupATLAS
+lsetup git
+
+1. Git clone and remote setup
+Clone my fork repository and set the atlas/athena.git as the upstream repository.
+
+git clone https://:@gitlab.cern.ch:8443/tkohno/athena.git
+cd athena
+git remote add upstream https://:@gitlab.cern.ch:8443/atlas/athena.git
+git remove -v
+
+2. Work with a branch
+Synchronize with the upstream repository and checkout the branch.
+
+git fetch upstream
+git checkout -b master-branch upstream/master --no-track
+
+3. Local commit
+
+git add -A (or specify each file)
+git status
+git commit -m 'some comment'
+git log --oneline <path/to/package>
+git show <commitId>
+git push --set-upstream origin master-branch
+
+4. Merge request
+
+5. Resolve conflict
+
+git fetch upstream
+git merge upstream/master
+
+"""
